ded173463de169b99ef8fe2f0df97bb447fe394a
Umair <umairwaraich2019@gmail.com>
2024-03-19 17:01:04 +0500

    add the proxy for backend server

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": "https://missmeta-backend.vercel.app",
+    },
+  },
   plugins: [react()],
   resolve: {
     alias: {
